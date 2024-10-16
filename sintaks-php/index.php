1412a0f0996e9c80411f8a0f13654c58478f10c0
enokrosanti <enokrosanti54@gmail.com>
2024-10-16 11:16:48 +0700

    sintaks php

---
+++ sintaks-php/index.php
@@ -0,0 +1,15 @@
+<?php
+    // Sintaks PHP
+
+    // Standar Output
+    // echo, print
+    // print_r [cetak array]
+    // var_dump [kayak console di js]
+
+    echo "Sandhika Galih";
+    echo 123;
+    echo true;
+    print "Sandhika Galih";
+    print_r("Sandhika Galih") ;
+    var_dump("Sandhika Galih");
+?>
