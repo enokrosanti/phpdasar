2dc4ff39a1144b2004a8384882b3482c6f5d0414
enokrosanti <enokrosanti54@gmail.com>
2024-10-16 11:40:16 +0700

    function: date

---
+++ function/date.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo date("l"); // tampil hari
+    echo date("d"); // tampil tanggal
+    echo date("M"); // tampil bulan dalam bentuk string
+    echo date("m"); // tampil bulan dalam bentuk angka
+    echo date("Y"); // tampil tahun
+
+    echo date("l, d-M-Y");
+
+    // dan lainnya...
+?>
