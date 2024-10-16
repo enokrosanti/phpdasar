79f0c53c208fa3fdacbe80e19b59381e5b41f907
enokrosanti <enokrosanti54@gmail.com>
2024-10-16 11:20:19 +0700

    pengulangan

---
+++ struktur-kendali/pengulangan.php
@@ -0,0 +1,22 @@
+<?php 
+
+// 1. for
+for($i = 0; $i < 5; $i++) {
+    echo "Hello World! <br>";
+};
+
+// 2. while
+$i = 0;
+while($i < 5) {
+    echo "Hello World! <br>";
+    $i++;
+};
+
+// 3. do...while
+$j = 0;
+do{
+    echo "Hello World! <br>";
+    $j++;
+} while ($j < 5);
+
+?>
